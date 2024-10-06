b0245e51c3600711a061f0b65568f847647ef639
Asif <107271220+Asif2902@users.noreply.github.com>
2024-10-07 01:25:58 +0600

    update server.js

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ app.use(bodyParser.json());
 // Setup multer for handling image uploads
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
-        const uploadDir = path.join(__dirname, 'public/uploads');
+        const uploadDir = path.join(__dirname, '/uploads');
         if (!fs.existsSync(uploadDir)) {
             fs.mkdirSync(uploadDir, { recursive: true });
         }
